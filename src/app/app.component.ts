51df1e553afb58baf9d8411b61bfbc566f80f857
Michelle Poterek and Max Scher <michelle@>
2017-08-15 11:08:18 -0700

    add sell-a-pint functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,25 @@ export class AppComponent {
     new Keg('Stout', 'Founders', 5, 8),
     new Keg('Rye', 'P.Diddy', 10, 4)
   ];
+
+  selectedKeg = null;
+
+  priceColor(currentKeg) {
+    if (currentKeg.price < 5) {
+      return "bg-info";
+    } else if (currentKeg.price < 10) {
+      return "bg-warning";
+    } else {
+      return "bg-success";
+    }
+  }
+
+  sellPint(keg) {
+    keg.remainingPints -= 1;
+  }
 }
 
 export class Keg {
   remainingPints: number = 124;
   constructor(public name: string, public brand: string, public price: number, public alcoholContent: number) {  };
-}
+};
