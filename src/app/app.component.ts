6e792c1559aeb3d9b7d45542e32898c0ab459864
Michelle Poterek and Max Scher <michelle@>
2017-08-15 10:22:00 -0700

    create constructor with three new kegs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'M&M\'s Taproom';
+  kegs: Keg[] = [
+    new Keg('IPA', 'Laurelwood', 4, 7),
+    new Keg('Stout', 'Founders', 5, 8),
+    new Keg('Rye', 'P.Diddy', 10, 4)
+  ];
+}
+
+export class Keg {
+  remainingPints: number = 124;
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: number) {  };
 }
