627cc03832dd6cc8c46b97e8bd70308f69004acd
Michelle Poterek and Max Scher <michelle@>
2017-08-16 15:03:43 -0700

    add functionality for addNewKeg, hiding form upon submission

--- src/app/new-keg/new-keg.component.ts
+++ src/app/new-keg/new-keg.component.ts
@@ -9,12 +9,13 @@ import { Keg } from './../keg.model';
 export class NewKegComponent implements OnInit {
   @Output() newKegSender = new EventEmitter();
 
+  addingKeg = false;
+
   submitForm(name: string, brand: string, price: number, abv: number) {
     var newKegToAdd: Keg = new Keg(name, brand, price, abv);
+    this.addingKeg = false;
     this.newKegSender.emit(newKegToAdd);
   }
-
-  addingKeg = false;
 
   ngOnInit() {
   }
